a76a43ce12f274dd794e530854e7400beb81e00e
Zusier <zusier@disroot.org>
2021-12-12 13:57:18 -0800

    v1

---
+++ assets/cursor.js
@@ -0,0 +1,39 @@
+consoleText('Zusier', 't1text', '#673ab7');
+
+function consoleText(word, id, color) {
+    if (color === undefined) color = ['white'];
+    var visible = true;
+    var con = document.getElementById('console');
+    var letterCount = 1;
+    var x = 1;
+    var waiting = false;
+    var target = document.getElementById(id)
+    target.setAttribute('style', 'color:' + color)
+    window.setInterval(function() {
+
+        if (letterCount === 0 && waiting === false) {
+            waiting = true;
+            target.innerHTML = word.substring(0, letterCount)
+            window.setTimeout(function() {
+                x = 1;
+                target.setAttribute('style', 'color:' + color)
+                letterCount += x;
+                waiting = false;
+            }, 1000)
+        } else if (waiting === false) {
+            target.innerHTML = word.substring(0, letterCount)
+            letterCount += x;
+        }
+    }, 160)
+    window.setInterval(function() {
+        if (visible === true) {
+            con.className = 'console-underscore hidden'
+            visible = false;
+
+        } else {
+            con.className = 'console-underscore'
+
+            visible = true;
+        }
+    }, 400)
+}
